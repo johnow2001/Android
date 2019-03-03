a76c4713e56a06611cc1ecff7af626843530af71
johnow2001 <46928542+johnow2001@users.noreply.github.com>
2019-03-03 17:27:56 +0000

    Story8 (#9)
    Story 8 Removed dummy tests

--- src/test/java/AndroidTest.java
+++ src/test/java/AndroidTest.java
@@ -133,38 +133,10 @@ public class AndroidTest {
 
     }
 
-    @Test
-    public void test2(){
-        //test code here
-    }
-
-    @Test
-    public void test3(){
-        //test code here
-    }
-
-    @Test
-    public void test4(){
-        //test code here
-    }
-
-    @Test
-    public void test5(){
-        //test code here
-    }
-
-    @Test
-    public void test6(){
-        //test code here
-    }
-    @Test
-
-    public void test7(){
-        //test code here
-    }
 
     @After
     public void afterTest() {
+        //the close is for browser
         //driver.close();
         driver.quit();
         //driver.toggleWifi();
